35e96a0dfc1aba004b48bc1ed67ae86378fb30c3
Victor Alessander <alessander@protonmail.com>
2017-10-29 02:09:23 -0300

    Removed old comment and implemented css when task is completed

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="todolist">
     <input type="text" v-model="newTodo.title" style="width: 400px;">
-    <!--<button @click=addTodo()>+</button>-->
     <input type="submit" class="button button-outline" @click="addTodo()" value="Add Task"></input>
     <ul style="padding: 0; list-style-type: none;">
       <li v-for="todo in todos" style="display: inline-block; margin: 0 10px;">
-        <label>{{ todo.title }} <input type="checkbox" v-model="todo.completed"></label>
+        <label v-bind:class="{done: todo.completed}">{{ todo.title }} <input type="checkbox" v-model="todo.completed"></label>
         <button @click="removeTodo(todo)" class="button-small">Remove</button>
       </li>
     </ul>
@@ -59,6 +58,11 @@ export default {
     height: 2.8rem;
     line-height: 2.8rem;
     padding: 0 1.5rem;
+  }
+
+  label.done {
+    color: #d9d9d9;
+    text-decoration: line-through;
   }
 
 </style>
